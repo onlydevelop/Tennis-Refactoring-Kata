cf9490197fe34d2c7365da98f662a475bf16538c
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-10-05 11:35:38 +0530

    T1: Rewrite the method as simple one-liner

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -32,28 +32,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getUnequalScore(String score) {
-        int tempScore;
-        for (int i = 1; i<3; i++)
-        {
-            if (i==1) tempScore = m_score1;
-            else { score +="-"; tempScore = m_score2;}
-            switch(tempScore)
-            {
-                case 0:
-                    score +="Love";
-                    break;
-                case 1:
-                    score +="Fifteen";
-                    break;
-                case 2:
-                    score +="Thirty";
-                    break;
-                case 3:
-                    score +="Forty";
-                    break;
-            }
-        }
-        return score;
+        return scoreLabel[m_score1] + "-" + scoreLabel[m_score2];
     }
 
     private String getHighScore() {
