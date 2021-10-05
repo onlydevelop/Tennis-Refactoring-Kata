ae774b1af798e68964962860d334ccae60ce4d6d
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-10-05 11:32:32 +0530

    T1: Replaced hardcoding with params

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
     private final String[] scoreLabel = {"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
@@ -59,10 +59,10 @@ public class TennisGame1 implements TennisGame {
     private String getHighScore() {
         String score;
         int minusResult = m_score1-m_score2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
+        if (minusResult==1) score ="Advantage " + player1Name;
+        else if (minusResult ==-1) score ="Advantage " + player2Name;
+        else if (minusResult>=2) score = "Win for " + player1Name;
+        else score ="Win for " + player2Name;
         return score;
     }
 
