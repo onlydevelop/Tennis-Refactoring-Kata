b1703b1821307adbbbe8c8c3e928862472083ec2
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-10-05 12:49:38 +0530

    T2: Removed hardcoding

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -81,21 +81,21 @@ public class TennisGame2 implements TennisGame
         
         if (p1point > p2point && p2point >= 3)
         {
-            score = "Advantage player1";
+            score = "Advantage " + player1Name;
         }
         
         if (p2point > p1point && p1point >= 3)
         {
-            score = "Advantage player2";
+            score = "Advantage " + player2Name;
         }
         
         if (p1point >=4 && p2point >=0 && (p1point - p2point)>=2)
         {
-            score = "Win for player1";
+            score = "Win for " + player1Name;
         }
         if (p2point >=4 && p1point >=0 && (p2point - p1point)>=2)
         {
-            score = "Win for player2";
+            score = "Win for " + player2Name;
         }
         return score;
     }
@@ -109,7 +109,7 @@ public class TennisGame2 implements TennisGame
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if (player == player1Name)
             incrementP1Score();
         else
             incrementP2Score();
